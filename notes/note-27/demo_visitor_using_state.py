53e747f6680ac681caddb58822065dfb4b67f7e4
Jskud <Jskud@Jskud.com>
2025-11-12 07:57:20 -0800

    publish note-27

---
+++ notes/note-27/demo_visitor_using_state.py
@@ -0,0 +1,239 @@
+"""Purpose: Show how to create a visitor function that uses local, not global, state"""
+
+def starting(msg):
+    print('\n' + msg)
+
+starting('''
+====================
+==== here we go ====
+====================
+''')
+
+data = range(8)
+
+def visit(sequence, visitor):
+    for item in sequence:
+        visitor(item)
+
+##================
+
+def demo_visit_using_print():
+    starting('==== demo_visit_using_print ====')
+    visit(data, print)
+
+##================
+
+"""Demo simple UGLY visitor using global variables"""
+
+suv_avg_count = None
+suv_avg_total = None
+
+def suv_setup():
+    global suv_avg_count, suv_avg_total
+    suv_avg_count = 0
+    suv_avg_total = 0
+
+def suv_visitor(item):
+    global suv_avg_count, suv_avg_total
+    suv_avg_count += 1
+    suv_avg_total += item
+
+def suv_fetch():
+    global suv_avg_count, suv_avg_total
+    return suv_avg_total / suv_avg_count
+    
+def suv_print():
+    suv_avg_fetch = suv_fetch()
+    print(f'{suv_avg_count=} {suv_avg_total=} {suv_avg_fetch=}')    
+
+def demo_simple_ugly_visitor():
+    starting('==== demo_simple_ugly_visitor ====')
+    suv_setup()
+    visit(data, suv_visitor)
+    suv_print()
+
+##================
+
+"""We define an object as a callable to allow our visitor function to have local state and expose results"""
+
+class Dynamic_Mean:
+
+    ## define and initialize all our attributes
+    ## I really like to see them all in one place,
+    ## so I know what I'm working with
+
+    _count = 0
+    _total = 0
+    _mean  = 0
+
+    ## NB: __init__ not needed!
+    ##     we initialize our object attributes from the class attributes; we also support reset
+
+    def __call__(self, item):
+        self._count += 1
+        self._total += item
+        self._mean = self._total/self._count
+
+    def reset(self):
+        ## NB: we could also use Dynamic_Mean.__init__(self)
+        self._count = 0
+        self._total = 0
+        self._mean  = 0
+
+def demo_visit_using_object():
+    starting('==== demo_visit_using_object ====')
+
+    v = Dynamic_Mean()
+    visit(data, v)
+    print(f'{v._count=} {v._total=} {v._mean=}')
+
+    v.reset()
+    visit(data, v)
+    print(f'{v._count=} {v._total=} {v._mean=}')
+    
+##================
+##================
+##================
+
+##================================================================
+##================================================================
+## Q/ How can we return a result from a visitor
+##    besides in global variables or an object?
+## A/ See below.
+##----------------------------------------------------------------
+## What is below here is too  complex for us right now
+## -- but it's here for me to maybe share with you later.
+## NB: These techniques are not something I would ordinarily use for this.
+##----------------------------------------------------------------
+## I really prefer an object to encapulate local state.
+## I really prefer an object to encapulate local state.
+## I really prefer an object to encapulate local state.
+##----------------------------------------------------------------
+##================================================================
+##================================================================
+
+##================
+
+"""We can define a local function using local variables to allow our visitor function to access local state"""
+
+def demo_visit_using_local():
+    starting('==== demo_visit_using_local ====')
+
+    count = 0
+    total = 0
+    mean = 0
+
+    ## NB: this is a 'one-shot' function -- we didn't provide a way to reset local state
+    def mean_visitor(item):
+        nonlocal count, total, mean
+        count += 1
+        total += item
+        mean = total/count
+
+    visit(data, mean_visitor)
+    print(f'{count=} {total=} {mean=}')
+
+##================
+
+"""We manufacture a function from a factory with local storage, but are unable to RETURN the result"""
+
+def demo_visit_using_factory():
+    starting('==== demo_visit_using_factory ====')
+
+    def make_noisy_visitor():
+        count = 0
+        total = 0
+        mean = 0
+        def mean_visitor(item):
+            nonlocal count, total, mean
+            count += 1
+            total += item
+            mean = total/count
+            print(f'{count=} {total=} {mean=}')
+
+        return mean_visitor
+
+    visitor = make_noisy_visitor()
+    visit(data, visitor)
+
+##================
+
+"""We manufacture a function using a factory, and give it the local storage to use.
+This lets us accumlate and RETURN the overall result from the visitor."""
+
+def make_mean_visitor_given_dict(d):
+    'NB: d is a dictionary, and is our storage'
+    assert type(d) == dict
+    d.clear()
+    d['count'] = 0
+    d['total'] = 0
+    d['mean'] = 0
+    def mean_visitor(item):
+        ## NB: `nonlocal d` not needed ;-) -- why not? 
+        d['count'] += 1
+        d['total'] += item
+        d['mean'] = d['total']/d['count']
+    return mean_visitor
+
+def demo_visit_given_dict():
+    starting('==== demo_visit_given_dict ====')
+    storage = {}
+
+    visitor = make_mean_visitor_given_dict(storage)
+    visit(data, visitor)
+    print(storage)
+
+    ## demo if avoid reset -- keeps accumulating
+    visit(data, visitor)
+    print(storage)
+
+    visitor = make_mean_visitor_given_dict(storage)
+    visit(data, visitor)
+    print(storage)
+
+##================
+
+"""We manufacture a function using a factory, and have it return the local storage it created and will use.
+This lets us accumlate and RETURN the overall result from the visitor."""
+
+def make_mean_visitor_fresh_dict():
+    'Return (visitor,dictionary)'
+    d = {}
+    d['count'] = 0
+    d['total'] = 0
+    d['mean'] = 0
+    def mean_visitor(item):
+        ## NB: `nonlocal d` not needed ;-) -- why not? 
+        d['count'] += 1
+        d['total'] += item
+        d['mean'] = d['total']/d['count']
+    return (mean_visitor,d)
+
+def demo_visit_fresh_dict():
+    starting('==== demo_visit_fresh_dict ====')
+
+    visitor,storage = make_mean_visitor_fresh_dict()
+    visit(data, visitor)
+    print(storage)
+
+    ## demo if avoid reset -- keeps accumulating
+    visit(data, visitor)
+    print(storage)
+
+    visitor,new_storage = make_mean_visitor_fresh_dict()
+    assert new_storage is not storage
+    visit(data, visitor)
+    print(new_storage)
+
+##================
+
+demo_visit_using_print()
+demo_simple_ugly_visitor()
+demo_visit_using_object()
+
+demo_visit_using_local()
+demo_visit_using_factory()
+demo_visit_given_dict()
+demo_visit_fresh_dict()
+
+[]
---
+++ notes/note-27/note-27.md
@@ -0,0 +1,158 @@
+# LC-CS-172 Topics for Wed 12-Nov-2025
+
+## Today's Hot Topics
+
+* midterm-2b addendum -- was *optional* -- with limited upside
+* why use objects
+* visitor pattern (details)
+* iterators (simple Python example)
+* pair programming w/ BST 
+
+## administrivia
+
+* please mark your attendance in Google Classroom for today
+
+* FYI: assignment #9 due date *is pushed out* to Mon 17-Nov-2025
+  - promised 2 hours in-class (120 minutes)
+  - 2 sessions so far -- ~70 minutes in-class remaining 
+
+## why use objects
+
+* Encapsulation and Modularity
+* Practical Model
+* Realistic Model
+* Intuitive Model
+* Data Hiding and Flexibility
+* Extensibility (Abstract Data Type)
+* Composable
+
+## Data Structures are Key
+
+	> Show me your flowcharts and conceal your tables,
+	> and I shall continue to be mystified.
+	> Show me your tables, and I won't usually need your flowcharts;
+	> they'll be obvious.
+	>
+	> > -- Fred Brooks, The Mythical Man-Month
+
+## visitor pattern (details)
+
+* `visit` method on collection
+* `visitor` function to operate on each element
+* examples
+  - print values in order
+  - find average of items
+  - verify BST integrity
+
+#### Issues w/ find-the-average visitor
+
+* needs [global] storage
+* needs many related functions (setup/accept/output)
+* names are independent, related by convention
+* pollutes global namespace
+
+#### Solution: Encapsulate -- Use an Object!
+
+* avoid *global **variables***  -- state unique to each object
+* related names of `storage` and `functions` are all together
+* avoids namespace pollution -- access items through object
+
+#### how to *return* info from a visitor
+
+* global variables (UGH!)
+* use objects (Yay!)
+* use a closure (Ooh!)
+
+[demo_visitor_using_state.py](demo_visitor_using_state.py)
+
+#### references for Design Patterns
+
+Design Patterns: Elements of Reusable Object-Oriented Software 1st Edition by
+Erich Gamma (Author), Richard Helm (Author), Ralph Johnson (Author), John
+Vlissides (Author), Grady Booch (Foreword)
+
+![Design Patterns](Design_Patterns.png)
+
+A Pattern Language: Towns, Buildings, Construction (Center for Environmental
+Structure Series) by Christopher Alexander (Author), Sara Ishikawa (Author),
+Murray Silverstein (Author), Max Jacobson (Author), Ingrid Fiksdahl-King
+(Author), Shlomo Angel (Author)
+
+![A Pattern Language](A_Pattern_Language.png)
+
+## iterators
+
+### BIG IDEA: function w/ state to return next item
+
+### simple Python example
+
+* Consider our simple linked list
+* Uses fascinating new feature -- **yield**
+
+	```
+    def __iter__(self):
+        ## defined as a generator function
+        here = self.head;
+        while (here != None):
+            yield here
+            here = here.next
+	```
+
+[demo_linked_list_iter.py](demo_linked_list_iter.py)
+
+# `***--->>> yield to in-class programming <<<---***`
+
+### Python details
+
+  - re: functions (aka routine, method)
+    - wo/ state (pure)
+	- w/ state  (better to use method on object, object holds state)
+     
+    - deterministic
+    - non-deterministic ('random')
+
+* uses **lazy** evaluation
+
+  - saves space
+  - may save time too
+
+* iterABLE
+
+  - provides an iterATOR
+
+* iterable and iterator as *class*
+
+  * `__iter__`	(iterable)
+  * `__next__`	(iterator)
+  * iter(iterable) returns iterator
+
+* generator *function*
+
+  - created when function itself includes `yield`
+
+* list comprehension
+
+  - syntax - NB: introducing *new* feature -- `if condition`
+
+	`expression for item in ITERABLE if condition`
+    
+  - uses square bracket notation [...]
+
+* generator expression
+
+  - like list comprehension,  
+    but elements one-at-a-time
+  - uses paren notation (...)
+
+* demo
+
+  - [demo_iterator.py](demo_iterator.py)
+  - TBD: [live-coding -- countdown iterator as object](demo_countdown_object.py)
+  - [demo_linked_list_iter.py](demo_linked_list_iter.py)
+  - [demo_linked_list_iter_test.py](demo_linked_list_iter_test.py)
+
+## Pair Programming w/ BST
+
+* work on CS-172 pair programming assignment #9
+
+#### []
